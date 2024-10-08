5563f03d8ea7905ba3966cac259dcda37e789044
Chanpreet02 <singhchanpreet883@gmail.com>
2024-10-08 13:02:54 +0530

    Program to print multiplication table of N upto 10

---
+++ lib/multiplication.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+void main(){
+  stdout.write("Enter the number: ");
+  int num = int.parse(stdin.readLineSync()!);
+  for(int i=1; i<=10; i++){
+    print("$num x $i = ${num * i}");
+  }
+}
